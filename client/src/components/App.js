a6815d975bda2c317b6df63c4f0b7503aad56e7a
Praveen Kumar <praveenscience@gmail.com>
2020-07-12 18:02:25 +0100

    Added Bootstrap CSS.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return <div>Hello GfG.</div>;
   }
 }
+
+export default App;
