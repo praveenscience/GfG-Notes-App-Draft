23f7b0ef44b111e608d523dbf6c4528469ac098f
Praveen Kumar <praveenscience@gmail.com>
2020-07-18 16:23:16 +0100

    Created navbar component.

---
+++ client/src/components/Shared/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = ({ dark, children, className }) => {
+  dark = !!dark ? "dark" : "light";
+  return (
+    <nav
+      className={
+        `Header navbar navbar-${dark} bg-${dark}` +
+        (className ? " " + className : "")
+      }
+    >
+      <span className="navbar-brand">{children}</span>
+    </nav>
+  );
+};
+
+export default Header;
