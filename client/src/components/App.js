a6eddac4417b4aba6e2836c02fdfb794a03a5b25
Praveen Kumar <praveenscience@gmail.com>
2020-07-18 16:13:08 +0100

    Created empty SCSS file for App.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>Hello GfG.</div>;
+    return <div className="App">Hello GfG</div>;
   }
 }
 
