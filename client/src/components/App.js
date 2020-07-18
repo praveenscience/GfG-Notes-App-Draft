ad5198cdf9aa0826955439072977ea0758aa4e6f
Praveen Kumar <praveenscience@gmail.com>
2020-07-18 17:50:11 +0100

    Added a router demo.
    * Introduction to React Routing.
    * Created a list of routes.
    * Intro to dynamic routes.
    * Router props with render.
    * Event handling with history.
    * Data rendering using consts.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,125 @@
 import React, { Component } from "react";
 import Header from "./Shared/Header";
+import { Switch, Route, Link } from "react-router-dom";
 
+const pages = [
+  {
+    Name: "Home",
+    Path: "/home"
+  },
+  {
+    Name: "About",
+    Path: "/about"
+  },
+  {
+    Name: "Login",
+    Path: "/login"
+  },
+  {
+    Name: "Users",
+    Path: "/users",
+    Exact: true,
+    DontRoute: true
+  },
+  {
+    Name: "User",
+    Path: "/users/:userId",
+    DontShow: true,
+    DontRoute: true
+  }
+];
+
+const users = [
+  "Praveen Kumar",
+  "Rohith",
+  "Kumar",
+  "Kshitij",
+  "Jinendra",
+  "Kranti",
+  "Vikrant"
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header dark={true}>Leadstagram</Header>
+        <Header dark={true} pages={pages}>
+          Leadstagram
+        </Header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Switch>
+                {pages
+                  .filter(a => !a.DontRoute)
+                  .map((item, key) => (
+                    <Route
+                      path={item.Path}
+                      key={key}
+                      exact={item.Exact}
+                      render={rp => (
+                        <>
+                          <h3>{item.Name}</h3>
+                          <p>
+                            I am in the {item.Name} page provided by{" "}
+                            <code>{item.Path}</code>.
+                          </p>
+                        </>
+                      )}
+                    />
+                  ))}
+                <Route
+                  path="/users"
+                  exact={true}
+                  render={() => (
+                    <>
+                      <h3>List of Users</h3>
+                      <div className="list-group">
+                        {users.map((item, key) => (
+                          <Link
+                            key={key}
+                            to={"/users/" + key}
+                            className="list-group-item list-group-item-action"
+                          >
+                            {item}
+                          </Link>
+                        ))}
+                      </div>
+                    </>
+                  )}
+                />
+                <Route
+                  path="/users/:userId"
+                  render={({ match, history }) => {
+                    const handleBack = () => {
+                      history.push("/users");
+                    };
+                    return (
+                      <>
+                        <h3>User: {users[match.params.userId]}</h3>
+                        <p>
+                          I am in the User:{" "}
+                          <strong>{users[match.params.userId]}</strong>'s page,
+                          who has an id of <code>{match.params.userId}</code>.
+                        </p>
+                        <button
+                          className="btn btn-info btn-sm"
+                          onClick={handleBack}
+                        >
+                          Back to List
+                        </button>
+                      </>
+                    );
+                  }}
+                />
+                <Route>
+                  <h3>Error 404</h3>
+                  <p>The page you are looking for is not found.</p>
+                </Route>
+              </Switch>
+              <p>This will be visible always! :)</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
