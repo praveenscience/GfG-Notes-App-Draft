2df40f85b1b29e48380d231831cab6dd0da2b057
Praveen Kumar <praveenscience@gmail.com>
2020-07-18 16:27:22 +0100

    Added header.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Header from "./Shared/Header";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello GfG</div>;
+    return (
+      <div className="App">
+        <Header dark={true}>Leadstagram</Header>
+      </div>
+    );
   }
 }
 
