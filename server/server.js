243dfa3cd74865f6cd827cbde74c6c3e050a0189
Praveen Kumar <praveenscience@gmail.com>
2020-07-25 17:38:15 +0100

    Created an Express Server.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("This is the server...");
+// Catch hold (import) of the Express Library.
+const express = require("express");
+// Now create an instance of the Express Library to do our app.
+const app = express();
+// Define a port for Express App to listen to.
+const port = 3100;
+
+app.get("/", (req, res) => {
+  console.log("Requested Path: " + req.path);
+  console.log("Requested Method: " + req.method);
+  console.log("Browser: " + req.headers["user-agent"]);
+
+  res.json({
+    Message: "Hello GfG!",
+    Error: false,
+    Headers: req.headers
+  });
+});
+
+// Listen to 3100 port.
+app.listen(port, () => {
+  console.log("Successfully started the server on port " + port);
+});
