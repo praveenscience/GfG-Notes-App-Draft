3aac7fa3d5f4000920443f24bce840f2d48a2ec8
Praveen Kumar <praveenscience@gmail.com>
2020-08-02 17:59:17 +0100

    Used nodemailer to send emails.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 // Catch hold (import) of the Express Library.
 const express = require("express");
-// Import morgan, cors and session.
+// Import morgan, cors, session and nodemailer.
 const morgan = require("morgan");
 const cors = require("cors");
 const session = require("express-session");
+const nodemailer = require("nodemailer");
 // Now create an instance of the Express Library to do our app.
 const app = express();
 // Define a port for Express App to listen to.
@@ -28,6 +29,26 @@ app.use(
 // Use the routes.
 app.use("/", root);
 app.use("/users", users);
+
+app.get("/email", async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "gfg4everyone@gmail.com",
+      pass: "qwerty@123"
+    }
+  });
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>',
+    to: "gfg4everyone@gmail.com",
+    subject: "Hello ✔ from Node JS!", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+  res.json({ info, previewUrl: nodemailer.getTestMessageUrl(info) });
+});
 
 // Listen to 3100 port.
 app.listen(port, () => {
