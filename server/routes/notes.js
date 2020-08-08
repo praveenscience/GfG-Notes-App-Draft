1afbbda99792ff56d680b191f2eb8e3eb51e8111
Praveen Kumar <praveenscience@gmail.com>
2020-08-08 17:06:03 +0100

    Implemented creation of a note.

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -23,8 +23,32 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   // Create a new note.
   // Check the POST request for all the required fields in req.body.
-  // Push the particular note.
-  res.json("ToDo.");
+  if (
+    req.body.title &&
+    req.body.title.trim().length > 3 &&
+    req.body.content &&
+    req.body.content.trim().length > 3 &&
+    typeof req.body.private === "boolean"
+  ) {
+    const { title, content, private } = req.body;
+    const username = req.session.User.username;
+    const editCount = 0,
+      createdAt = (updatedAt = new Date());
+    // Push the particular note.
+    res.json(
+      notes.push({
+        username,
+        title,
+        content,
+        private,
+        editCount,
+        createdAt,
+        updatedAt
+      }) - 1
+    );
+  } else {
+    res.status(400).json("Sorry, you need both title and content.");
+  }
 });
 
 module.exports = app;
