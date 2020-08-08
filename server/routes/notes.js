37621614ec516f001177d873fecca1936b73f0a1
Praveen Kumar <praveenscience@gmail.com>
2020-08-08 16:46:54 +0100

    Show routes only when authorised.

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -6,8 +6,20 @@ const notes = [];
 
 // List all notes.
 app.get("/", (req, res) => {
-  // Show all the notes.
-  res.json(notes);
+  // Show all the notes if user is logged in.
+  if (!!req.session.User) {
+    res.json(notes);
+  } else {
+    res.status(401).json("You need to login to see all notes!");
+  }
+});
+
+// Create a new note.
+app.post("/", (req, res) => {
+  // Create a new note.
+  // Check the POST request for all the required fields in req.body.
+  // Push the particular note.
+  res.json("ToDo.");
 });
 
 module.exports = app;
