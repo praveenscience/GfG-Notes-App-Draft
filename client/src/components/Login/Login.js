dd2e809b3557e03421e7e5c24054dad12d26aa9c
Praveen Kumar <praveenscience@gmail.com>
2020-08-09 17:47:15 +0100

    Implemented Registration Form.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,28 +2,42 @@ import React, { useState } from "react";
 import FormGroup from "../Forms/FormGroup";
 
 const Login = ({ handleAuthentication, Error }) => {
-  const [FormData, setFormData] = useState({
+  const [LoginData, setLoginData] = useState({
     username: "",
     password: ""
   });
-  const handleChange = e => {
-    setFormData({
-      ...FormData,
+  const [RegisterData, setRegisterData] = useState({
+    username: "",
+    password: "",
+    fullname: ""
+  });
+  const handleLoginData = e => {
+    setLoginData({
+      ...LoginData,
       [e.target.name]: e.target.value
     });
   };
-  const handleSubmit = e => {
+  const handleRegisterData = e => {
+    setRegisterData({
+      ...RegisterData,
+      [e.target.name]: e.target.value
+    });
+  };
+  const handleLogin = e => {
     e.preventDefault();
-    handleAuthentication({ ...FormData });
+    handleAuthentication({ ...LoginData });
+  };
+  const handleRegister = e => {
+    e.preventDefault();
   };
   return (
     <div className="container my-5">
       <div className="row">
-        <div className="col-12 col-md-6 offset-md-3">
+        <div className="col-12 col-md-6">
           <div className="card">
             <h5 className="card-header">Sign In</h5>
             <div className="card-body">
-              <form onSubmit={handleSubmit}>
+              <form onSubmit={handleLogin}>
                 {Error && (
                   <div className="alert alert-danger text-center">
                     Username or password is wrong!
@@ -34,15 +48,15 @@ const Login = ({ handleAuthentication, Error }) => {
                     Id: "username",
                     Label: "Username",
                     Type: "text",
-                    onChange: handleChange,
-                    Value: FormData.username
+                    onChange: handleLoginData,
+                    Value: LoginData.username
                   },
                   {
                     Id: "password",
                     Label: "Password",
                     Type: "password",
-                    onChange: handleChange,
-                    Value: FormData.password
+                    onChange: handleLoginData,
+                    Value: LoginData.password
                   }
                 ].map((fg, key) => (
                   <FormGroup {...fg} key={key} />
@@ -50,6 +64,50 @@ const Login = ({ handleAuthentication, Error }) => {
                 <input
                   type="submit"
                   value="Sign In"
+                  className="btn btn-primary"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <div className="card">
+            <h5 className="card-header">New User? Register</h5>
+            <div className="card-body">
+              <form onSubmit={handleRegister}>
+                {Error && (
+                  <div className="alert alert-danger text-center">
+                    Username already exists!
+                  </div>
+                )}
+                {[
+                  {
+                    Id: "username",
+                    Label: "Username",
+                    Type: "text",
+                    onChange: handleRegisterData,
+                    Value: RegisterData.username
+                  },
+                  {
+                    Id: "password",
+                    Label: "Password",
+                    Type: "password",
+                    onChange: handleRegisterData,
+                    Value: RegisterData.password
+                  },
+                  {
+                    Id: "fullname",
+                    Label: "Full Name",
+                    Type: "text",
+                    onChange: handleRegisterData,
+                    Value: RegisterData.fullname
+                  }
+                ].map((fg, key) => (
+                  <FormGroup {...fg} key={key} />
+                ))}
+                <input
+                  type="submit"
+                  value="Register"
                   className="btn btn-primary"
                 />
               </form>
