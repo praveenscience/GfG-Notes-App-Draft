ab6c33d9d5f6182f4fcb9b34b2568444e191bb14
Praveen Kumar <praveenscience@gmail.com>
2020-08-15 17:05:59 +0100

    Removed unnecessary Link import.

--- client/src/components/Notes/_NotesContainer.js
+++ client/src/components/Notes/_NotesContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useRouteMatch } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom";
 
 const NotesContainer = () => {
   const urm = useRouteMatch();
