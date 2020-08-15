b4ed3c14b3ea1c7c88c4930e6ef4a9541c926abb
Praveen Kumar <praveenscience@gmail.com>
2020-08-15 16:11:28 +0100

    Converted Notes into Class Component.

--- client/src/components/Notes/Notes.js
+++ client/src/components/Notes/Notes.js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Notes = ({ LoggedIn, Logout }) => {
-  return (
-    <div className="container-fluid my-5">
-      <div className="row">
-        <div className="col-12">
-          <div className="card">
-            <h5 className="card-header">
-              <span className="align-middle d-inline-block mt-1">
-                Welcome, {LoggedIn.fullname}!
-              </span>
-              <button
-                className="btn btn-danger btn-sm float-right"
-                onClick={Logout}
-              >
-                Logout
-              </button>
-            </h5>
-            <div className="card-body">
-              <div className="row">
-                <div className="col-2">
-                  <p>List all Notes</p>
-                </div>
-                <div className="col-10">
-                  <pre>{JSON.stringify(LoggedIn, null, 2)}</pre>
+class Notes extends Component {
+  render() {
+    const { LoggedIn, Logout } = this.props;
+    return (
+      <div className="container-fluid my-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <h5 className="card-header">
+                <span className="align-middle d-inline-block mt-1">
+                  Welcome, {LoggedIn.fullname}!
+                </span>
+                <button
+                  className="btn btn-danger btn-sm float-right"
+                  onClick={Logout}
+                >
+                  Logout
+                </button>
+              </h5>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-2">
+                    <p>List all Notes</p>
+                  </div>
+                  <div className="col-10">
+                    <pre>{JSON.stringify(LoggedIn, null, 2)}</pre>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Notes;
