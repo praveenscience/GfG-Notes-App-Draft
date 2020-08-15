e0ac1249e88d8d0279a71bf7beee3277fee8ed24
Praveen Kumar <praveenscience@gmail.com>
2020-08-15 15:49:44 +0100

    Health check API service added.

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -5,3 +5,5 @@ export const AuthenticateUser = cred => Axios.post("/api/users/login", cred);
 export const LogoutUser = () => Axios.post("/api/users/logout");
 
 export const RegisterUser = data => Axios.post("/api/users/new", data);
+
+export const GetActiveUser = () => Axios.get("/api/users/login");
