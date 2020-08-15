8fa1a633df26f562ccd7eebb250b6706c0d5cde2
Praveen Kumar <praveenscience@gmail.com>
2020-08-15 17:58:13 +0100

    Rendered notes to be viewed.

--- client/src/components/Notes/Note.js
+++ client/src/components/Notes/Note.js
@@ -1,10 +1,31 @@
 import React from "react";
 
+/*
+{
+  "username": "Praveen",
+  "title": "First Note",
+  "content": "GfG Classes are going to end!",
+  "private": false,
+  "editCount": 0,
+  "createdAt": "2020-08-15T15:16:50.936Z",
+  "updatedAt": "2020-08-15T15:16:50.936Z"
+}
+*/
+
 const NoteContent = ({ Note }) => {
   return (
-    <pre className="border rounded bg-light p-3">
-      {JSON.stringify(Note, null, 2)}
-    </pre>
+    <section className="NotesContainer-Content">
+      <header>
+        <h3>{Note.title}</h3>
+        <div className="Author">
+          <p>
+            Written by <em>{Note.username}</em> on <em>{Note.createdAt}</em>.
+            Last updated on <em>{Note.updatedAt}</em>.
+          </p>
+        </div>
+      </header>
+      <article className="border rounded p-3 bg-light">{Note.content}</article>
+    </section>
   );
 };
 
--- client/src/components/Notes/_NotesContainer.js
+++ client/src/components/Notes/_NotesContainer.js
@@ -8,8 +8,8 @@ const NotesContainer = ({ Notes }) => {
   const isHome = urm.path === "/";
   const Note = !isHome ? Notes[+urm.params.NoteID.replace("note-", "")] : null;
   return (
-    <div className="col-10">
-      {isHome ? <NotesHome /> : <NoteContent Note={Note} />}
+    <div className="NotesContainer col-10">
+      {!isHome && Note ? <NoteContent Note={Note} /> : <NotesHome />}
     </div>
   );
 };
