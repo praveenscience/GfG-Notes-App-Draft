2870c8699a114b39ff517ba9b06e77096c3905c8
Praveen Kumar <praveenscience@gmail.com>
2020-08-16 17:28:48 +0100

    Created service call for new note.

--- client/src/services/NotesService.js
+++ client/src/services/NotesService.js
@@ -2,4 +2,6 @@ import Axios from "axios";
 
 export const GetAllNotes = () => Axios.get("/api/notes");
 
+export const CreateNote = Note => Axios.post("/api/notes", Note);
+
 export const DeleteNote = NoteID => Axios.delete("/api/notes/" + NoteID);
