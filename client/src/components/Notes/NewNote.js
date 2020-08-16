b5ddcdc3df02cffe3f53a46fd2e8ca469bf828a7
Praveen Kumar <praveenscience@gmail.com>
2020-08-16 17:29:50 +0100

    Added functionality to redirect users after save.

--- client/src/components/Notes/NewNote.js
+++ client/src/components/Notes/NewNote.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import { withRouter } from "react-router-dom";
 
-const NewNote = ({ LoggedIn, CreateNote }) => {
+const NewNote = ({ LoggedIn, CreateNote, history }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("*Start writing your note...*");
   const [priv, setPriv] = useState(false);
+  const [Saved, setSaved] = useState(false);
   const handleCreateNote = e => {
     e.preventDefault();
     CreateNote({
@@ -12,7 +14,11 @@ const NewNote = ({ LoggedIn, CreateNote }) => {
       content,
       private: priv
     });
+    setSaved(true);
   };
+  if (Saved) {
+    history.push("/");
+  }
   return (
     <section className={"NotesContainer-Content" + (priv ? " private" : "")}>
       <header>
@@ -52,4 +58,4 @@ const NewNote = ({ LoggedIn, CreateNote }) => {
   );
 };
 
-export default NewNote;
+export default withRouter(NewNote);
