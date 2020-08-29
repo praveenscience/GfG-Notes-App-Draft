5ddf92af851b2f45b3928707bb06a21f464112d5
Praveen Kumar <praveenscience@gmail.com>
2020-08-29 17:13:49 +0100

    Added a Service Call for Edit Note.

--- client/src/services/NotesService.js
+++ client/src/services/NotesService.js
@@ -5,5 +5,8 @@ export const GetAllNotes = () => Axios.get(APIHost + "/api/notes");
 
 export const CreateNote = Note => Axios.post(APIHost + "/api/notes", Note);
 
+export const UpdateNote = (NoteID, Note) =>
+  Axios.post(APIHost + "/api/notes/" + NoteID, Note);
+
 export const DeleteNote = NoteID =>
   Axios.delete(APIHost + "/api/notes/" + NoteID);
